5e6a72b2387444334b70b4d05a2c70168aa61476
Tracey <Tracey.brosnan@students.ittralee.ie>
2021-05-05 20:40:14 +0100

    ADding mapping

--- src/main/java/com/cloudappdev/aws/WelcomeController.java
+++ src/main/java/com/cloudappdev/aws/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.cloudappdev.aws;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController{
 
+    @GetMapping
     public String welcome(){
         return "Welcome to Cloud Application Development!";
     }
