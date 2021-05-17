54ba7d198871ebbaf11a49cacbeb94f361bce795
Tracey <Tracey.brosnan@students.ittralee.ie>
2021-05-17 16:22:53 +0100

    New Build

--- src/main/java/com/cloudappdev/aws/WelcomeController.java
+++ src/main/java/com/cloudappdev/aws/WelcomeController.java
@@ -1,5 +1,4 @@
 package com.cloudappdev.aws;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/welcome")
 @RestController
 public class WelcomeController{
-
     @GetMapping
     public String welcome(){
-        return "<h1>Welcome to Cloud Application Development! /n We used the Following Cloud services: ElasticBeanstalk, EC2,S3, CodeBuild, CodeDeploy and Cloudwatch<h1/>";
-
+        return "<h1>Welcome to Cloud Application Development! /n This application used the Following Cloud services: ElasticBeanstalk, EC2,S3, CodeBuild, CodeDeploy and Cloudwatch<h1/>";
 
     }
-
 }
